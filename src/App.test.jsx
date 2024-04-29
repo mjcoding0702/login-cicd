e95bfb720cd0954ec1f287cab100a610d8ceec8a
mjcoding0702 <chungmangjie2000@gmail.com>
2024-04-29 12:53:52 +0800

    change to check button is disabled

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -16,6 +16,6 @@ describe("App form submission", () => {
     await userEvent.type(emailInput, 'test@');
     
     // Check if the button is not disabled
-    expect(submitButton.disabled).toBe(true); 
+    expect(submitButton.disabled).toBe(false); 
   });
 });
