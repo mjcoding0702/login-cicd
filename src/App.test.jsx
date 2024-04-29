b8e9581a92d091beb0fd4397ca01a9735f0a8fba
mjcoding0702 <chungmangjie2000@gmail.com>
2024-04-29 13:00:39 +0800

    change to fail case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -15,7 +15,7 @@ describe("App form submission", () => {
     // Type a valid email into the input
     await userEvent.type(emailInput, 'test@');
     
-    // Check if the button is not disabled
+    // Failed case
     expect(submitButton.disabled).toBe(false); 
   });
 });
